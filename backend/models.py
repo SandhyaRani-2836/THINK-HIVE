a3054f816421e6897d6e6ad2c238d47f00a99a3f
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-05 19:02:45 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,12 @@
-Databse schema
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_name = db.Column(db.String(100), nullable=False)
+    department = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    problem_statement = db.Column(db.Text, nullable=False)
+    drawbacks = db.Column(db.Text)
+    code_link = db.Column(db.String(300))
