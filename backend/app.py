8efeee45164e9f12b87b46d6d47c0b8cd1905c51
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-05 14:43:08 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='../frontend/static', template_folder='../frontend')
+# Initialize Flask app with specific static and template folder paths
+app = Flask(__name__, static_folder="frontend/static", template_folder="frontend/templates")
 
+# Define a route for the home page
 @app.route('/')
 def home():
-    return render_template('index.html', project_name="THINK HIVE")
+    return render_template('index.html')
 
+# Run the application
 if __name__ == '__main__':
     app.run(debug=True)
