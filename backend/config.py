8b7d47e2225ff4249ac9bd30ae8bd5b73dc5dff0
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-05 18:42:24 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,5 @@
-configuration settings
+import os
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://username:password@localhost/db_name")
