c0f89967ee605118d5d1d597e984b98db6e8db9c
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-05 14:17:48 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -11,20 +11,19 @@ def home():
 @app.route('/verify_project', methods=['POST'])
 def verify_project():
     if request.method == 'POST':
-        project_name = "THINK HIVE"
+        # Retrieve project details from the form
+        project_name = request.form['project_name']
         project_description = request.form['project_description']
-        # You can add the logic to save this data to the database here
         return redirect(url_for('home'))
-    return render_template('verify_project.html', project_name="THINK HIVE")
+    return render_template('verify_project.html')
 
 # Route for submitting code
 @app.route('/submit_code', methods=['POST'])
 def submit_code():
     if request.method == 'POST':
         code = request.form['code']
-        # Save the code to the database, if required
         return redirect(url_for('home'))
-    return render_template('submit_code.html', project_name="THINK HIVE")
+    return render_template('submit_code.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
