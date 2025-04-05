b6f39f0d33d2ba7d9adb3c0e32f180b08c6d8a24
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-05 14:30:31 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,10 @@
-from flask import Flask, render_template, send_from_directory
-import os
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='frontend')
+app = Flask(__name__, static_folder='../frontend/static', template_folder='../frontend')
 
 @app.route('/')
 def home():
     return render_template('index.html', project_name="THINK HIVE")
-
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory(os.path.join(app.root_path, 'frontend'), filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
