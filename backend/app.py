3c63a88825766ca88f6c67ecc3dc5c72dfed9c98
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-05 12:20:48 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
-app.py
+from flask import Flask
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
