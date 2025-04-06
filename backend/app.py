bb7027ee170f2b1fb5a250b0de8e5a1844278364
Sharanya-8 <sharanyaalugani811@gmail.com>
2025-04-07 00:19:02 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,8 @@
 from flask import Flask
-from config import Config
-from models import db
 from routes import routes
 
-app = Flask(_name_, static_folder=\"../frontend\", template_folder=\"../frontend\")
-app.config.from_object(Config)
-
-db.init_app(app)
+app = Flask(_name_)
 app.register_blueprint(routes)
 
 if _name_ == '_main_':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
