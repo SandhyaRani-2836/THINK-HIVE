9c7dc58ffbe7b4a6d34f8701f7cc73c092686b00
Sharanya-8 <sharanyaalugani811@gmail.com>
2025-04-09 00:15:26 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from routes import routes
 import os
 
-app = Flask(_name_)
+app = Flask(_name_, template_folder='../frontend/templates', static_folder='../frontend/static')
 app.secret_key = "thinkhive"
 app.config['UPLOAD_FOLDER'] = 'uploaded_documents'
 
