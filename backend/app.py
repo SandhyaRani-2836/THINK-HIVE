f149b9476ad9bb3ae53907ecbb4b679de0d5c8b6
Sharanya-8 <sharanyaalugani811@gmail.com>
2025-04-08 23:58:59 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,12 @@
-from flask import Flask, render_template
-from config import Config
-from models import db
+from flask import Flask
 from routes import routes
+import os
 
-app = Flask(__name__, static_folder="../frontend", template_folder="../frontend")
-app.config.from_object(Config)
+app = Flask(_name_)
+app.secret_key = "thinkhive"
+app.config['UPLOAD_FOLDER'] = 'uploaded_documents'
 
-db.init_app(app)
 app.register_blueprint(routes)
 
-@app.route('/')
-def home():
-    return app.send_static_file('index.html')
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+if _name_ == "_main_":
     app.run(debug=True)
