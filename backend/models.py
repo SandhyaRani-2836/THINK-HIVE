f72165b4f5e7304dff8002172a67b852c63de00c
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-17 14:37:27 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -2,11 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    department = db.Column(db.String(80), nullable=False)
-    topic = db.Column(db.String(120), nullable=False)
-    code_link = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    submitted_by = db.Column(db.String(120), nullable=False)
+    code_file = db.Column(db.String(200), nullable=False)
