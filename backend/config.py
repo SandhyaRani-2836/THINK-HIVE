378e24babeadd65102cc9f88b4f5c04184644953
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-17 14:34:27 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,7 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(_file_), '..', 'database'))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, \"database.db\")}'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE_PATH = os.path.join(os.path.dirname(BASE_DIR), 'database', 'thinkhive.db')
+SECRET_KEY = 'your_secret_key'
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{DATABASE_PATH}'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
