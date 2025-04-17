b2d4d77d6962119d10db65fada19620ef41fe5aa
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-17 14:38:58 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,4 @@
-from flask import Flask
-from routes import routes
-import os
+from routes import app
 
-app = Flask(__name__, 
-            template_folder='templates', 
-            static_folder='static')
-
-app.secret_key = "thinkhive"
-app.config['UPLOAD_FOLDER'] = 'uploaded_documents'
-
-app.register_blueprint(routes)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
