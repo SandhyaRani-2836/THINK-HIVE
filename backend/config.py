c950f80ecd9454835d6eecce6e53d7a99210bce5
SandhyaRani-2836 <gugulothusandhyarani0@gmail.com>
2025-04-17 14:51:43 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE_PATH = os.path.join(os.path.dirname(BASE_DIR), 'database', 'database.db')  # updated here
-SECRET_KEY = 'your_secret_key'
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "database.db")}'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "database.db")}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
